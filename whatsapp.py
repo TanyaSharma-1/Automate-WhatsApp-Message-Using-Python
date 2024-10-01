c5756127b0d750ea5339c53176acec03d9b3c8cf
Tanya Sharma <tanyasharma827x@gmail.com>
2024-10-01 12:14:42 +0200

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -1,6 +1,6 @@
 # This is an Automate Whatsapp Messanger Mini Python Project 
 
-
 import pywhatkit
 
 pywhatkit.sendwhatmsg("+91__________","Type The Message Here",15,33)  # 15 = hour, 33 = minutes
+
